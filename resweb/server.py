34232e9f66d915a692bb67290c7d2c9f0e72123d
aezell <alex@myemma.com>
2010-03-23 00:22:32 -0500

    Allow user to requeue or delete failed jobs.
    Using resweb, the user can click a button for each failed
    job and either requeue or delete that job. The job should
    be put back into its original queue if requeued.
    
    The templates might need a little work, but they are
    functional.

--- resweb/server.py
+++ resweb/server.py
@@ -40,6 +40,33 @@ def failed(request):
     start = request.GET.get('start',0)
     start = int(start)
     return str(Failed(HOST, start).render())
+    
+@post('/failed/retry/')
+def failed_retry(request):
+    from base64 import b64decode
+    from pyres import failure
+    try:
+        import json
+    except ImportError:
+        import simplejson as json
+    failed_job = request.POST['failed_job']
+    job = b64decode(failed_job)
+    decoded = ResQ.decode(job)
+    failure.retry(HOST, decoded['queue'], job)
+    raise Redirect('/failed/')
+    
+@post('/failed/delete/')
+def failed_delete(request):
+    from base64 import b64decode
+    from pyres import failure
+    try:
+        import json
+    except ImportError:
+        import simplejson as json
+    failed_job = request.POST['failed_job']
+    job = b64decode(failed_job)
+    failure.delete(HOST, job)
+    raise Redirect('/failed/')
 
 @get('/workers/(?P<worker_id>\w.+)/')
 def worker(request, worker_id):
@@ -63,7 +90,6 @@ def stat(request, stat_id):
 
 @get('/media/(?P<filename>.+)')
 def my_media(request, filename):
-    print filename
     #return serve_static_file(request, filename)
     #my_media.content_type = content_type(filename)
     
